34bffe8739e1d5479992ddceff0ca73df87a7e45
adelacanija229-source <adelacanija229@gmail.com>
2026-03-23 10:38:22 +0900

    Add files via upload

---
+++ dist/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'adela-sync-v1.0.2'; // 버전 번호를 올리면 업데이트가 감지됩니다.
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // 즉시 활성화
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== CACHE_NAME;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 업데이트 알림을 위한 메시지 리스너
+self.addEventListener('message', (event) => {
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
